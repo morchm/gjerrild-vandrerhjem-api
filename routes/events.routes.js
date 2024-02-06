76f57a9c95a7c7bf7ed6f0c6044c67c8e8e37103
morchm <mai.lykke.moerch@gmail.com>
2024-02-06 11:58:57 +0100

    Update events.routes.js

--- routes/events.routes.js
+++ routes/events.routes.js
@@ -9,7 +9,7 @@ router.get( '/', async (req, res) => {
     console.log ("Events - GET/Hent")
     try {
         const event = await Events.find()
-        res.status( 200 ).json( event )
+        res.status( 200 ).json( {event} )
     } catch (error) {
         res.status( 500 ).json( { message: "Der er opstået en fejl ved GET" } )
     }
